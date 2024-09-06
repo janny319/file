278dda8ef625c80ed420915a9d39e3e9ecc9ff4a
DESKTOP-H74K4NM\Frameout <janny84@frameout.co.kr>
2024-09-06 17:59:44 +0900

    포트폴리오 페이지 작업중

--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -1,11 +1,47 @@
-import React from "react";
-import portfolioData from "dataJson/portfolioData.json"
+import React, { useRef} from "react";
+import portfolioData from "dataJson/portfolioData.json";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Portfolio = () => {
+    const list = useRef()
+
+    useGSAP(() => {
+        const items = gsap.utils.toArray('.portfolid-box__item');
+
+        items.forEach((item) => {
+            gsap.to(item, {
+                x: 150,
+                scrollTrigger: {
+                  trigger: item,
+                  start: 'bottom bottom',
+                  end: 'top 20%',
+                  scrub: true,
+                  // markers: true,
+                },
+              });
+            // gsap.fromTo(
+            //     item,
+            //     { opacity:0, y: 50 },
+            //     { opacity: 1, y: 0, duration: 0.5,
+            //         scrollTrigger: {
+            //             trigger: item,
+            //             start: 'top 80%',
+            //             end: 'top 50%',
+            //         }
+            //     },
+            // )
+        });
+    },
+    {scope: list}
+);
+
     return(
         <div className="portfolio">
             <div className="portfolio-box">
-                <ul className="portfolio-box__list">
+                <ul className="portfolio-box__list" ref={list}>
                     {portfolioData.map((item) => (
                         <li key={item.id} className="portfolio-box__item">
                             <a href="#none">
