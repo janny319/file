148137aaa7a321d6954d572cf2642ebe8737b6c8
DESKTOP-H74K4NM\Frameout <janny84@frameout.co.kr>
2024-09-13 14:03:26 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import Layout from "layout/Layout";
 
 import 'styles/style.scss'
 
 function App() {
     return (
+    <BrowserRouter basename={process.env.PUBIL_URL}>
         <Router>
             <Routes>
                 <Route path='/' element={<Layout />}></Route>
             </Routes>
         </Router>
+    </BrowserRouter>
     );
 }
 
